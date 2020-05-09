5e34c6023db9489e60eebf3b4c818430113f66a3
Saksham-Singhal-hash <63529173+Saksham-Singhal-hash@users.noreply.github.com>
2020-05-09 17:39:34 +0530

    Add files via upload

---
+++ baseclass.js
@@ -0,0 +1,18 @@
+// class baseclass{
+//     constructor(x,y,width,height){
+//         var option = {
+//             "restitution":0.8,
+//             "friction":1.0,
+//             "density":1.0
+//         }
+//         this.body = Bodies.rectangle(x,y,width,height,options);
+//         this.width=width;
+//         this.height;
+//         world.add(world,this.body);
+//     }
+//     display(){
+//         Push
+//         translate(this.body.position.x,this.body.position.y);
+//         pop
+//     }
+// }
